a59b2396fc62a25c9ec653d0f459197d2eef275f
Iago Virgílio <iagovirgilio@gmail.com>
2024-03-03 10:03:44 -0300

    Configurar PostgreSQL

--- app/currency_convert/settings.py
+++ app/currency_convert/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'currency_convert.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE', 'change-me'),
+        'NAME': os.getenv('POSTGRES_DB', 'change-me'),
+        'USER': os.getenv('POSTGRES_USER', 'change-me'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'change-me'),
+        'HOST': os.getenv('POSTGRES_HOST', 'change-me'),
+        'PORT': os.getenv('POSTGRES_PORT', 'change-me'),
     }
 }
 
